74b45f29a04381417e279765683dae4edec7d791
surymy <66707438+surymy@users.noreply.github.com>
2022-06-29 21:36:08 +0800

    Add files via upload

--- my-vue-manage/src/main.js
+++ my-vue-manage/src/main.js
@@ -3,14 +3,24 @@ import App from './App.vue'
 // 引入element-ui插件极其样式
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+// 引入css样式表
+import './assets/less/index.less'
 
 // 引入router
 import router from './router'
+//  引入 store
+import store from './store'
+// 引入axios
+import http from 'axios'
+// 引入mock
+import '../api/mock.js'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.prototype.$http = http
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
